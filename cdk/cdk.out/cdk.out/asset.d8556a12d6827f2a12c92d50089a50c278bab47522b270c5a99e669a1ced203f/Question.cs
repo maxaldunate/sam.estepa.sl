4248ceb3110c6e5ed979f249fc9d28bff6b28c8b
maxaldunate <maxaldunate@gmail.com>
2020-06-01 20:42:03 +0200

    remove cdk out folder

--- cdk/cdk.out/cdk.out/asset.d8556a12d6827f2a12c92d50089a50c278bab47522b270c5a99e669a1ced203f/Question.cs
+++
@@ -1,9 +0,0 @@
-namespace PostQuestionsService
-{
-    class Question
-    {
-        public string Id { get; internal set; }
-        public string Text { get; internal set; }
-        public string EmailAddress { get; internal set; }
-    }
-}
