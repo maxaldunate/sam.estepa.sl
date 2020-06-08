bd2a15829b12802dab6d1d0b62b4c7b32e285bb7
maxaldunate <maxaldunate@gmail.com>
2020-06-08 18:39:08 +0200

    improve output

--- infra/deploy_frontend.sh
+++ infra/deploy_frontend.sh
@@ -12,7 +12,7 @@ S3_BUCKET_NAME="$PROJECT_NAME-frontend-$(aws sts get-caller-identity --query Acc
 
 cd ${FRONTEND_PATH} && \
 
-echo -------------Variables:
+echo -------------Variables
 echo PROJECT_NAME        $PROJECT_NAME
 echo AWS_PROFILE         $AWS_PROFILE
 echo DIR                 $DIR
@@ -20,18 +20,18 @@ echo FRONTEND_PATH       $FRONTEND_PATH
 echo FRONTEND_BUILD_PATH $FRONTEND_BUILD_PATH
 echo AWS_REGION          $AWS_REGION
 echo S3_BUCKET_NAME      $S3_BUCKET_NAME
-
-echo ------------- Building frontend:
+echo
+echo ------------- Building frontend
 # # npm run build -- --prod
 node_modules/@angular/cli/bin/ng build --prod
-
-echo ------------- Creation and Copying to bucket:
+echo
+echo ------------- Creation and Copying to bucket
 aws s3 $AWS_PROFILE mb s3://$S3_BUCKET_NAME --region $AWS_REGION || true
 aws s3 $AWS_PROFILE website s3://$S3_BUCKET_NAME --index index.html --error index.html
 aws s3 $AWS_PROFILE rm s3://$S3_BUCKET_NAME --recursive
 aws s3 $AWS_PROFILE cp $FRONTEND_BUILD_PATH s3://$S3_BUCKET_NAME --acl public-read --recursive
 S3_BUCKET_LOCATION=$(aws s3api $AWS_PROFILE get-bucket-location --bucket $S3_BUCKET_NAME --query LocationConstraint --output text)
-
+echo
 echo ------------- Output:
 echo S3_BUCKET_LOCATION      $S3_BUCKET_LOCATION
 if [ ! $S3_BUCKET_LOCATION = "None" ]; then
@@ -39,3 +39,7 @@ if [ ! $S3_BUCKET_LOCATION = "None" ]; then
 else
     echo "View your project here: http://$S3_BUCKET_NAME.s3-website.us-east-1.amazonaws.com"
 fi
+
+echo
+echo ------------- Check:
+aws s3api list-buckets --query 'Buckets[?starts_with(Name, `sam-estepa`) == `true`]' $AWS_PROFILE
