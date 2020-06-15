e3f77573a771476a2d1e5e2997d4c209ec79221c
maxaldunate <maxaldunate@gmail.com>
2020-06-15 19:33:28 +0200

    improve deploy

--- infra/frontend_deploy.sh
+++ infra/frontend_deploy.sh
@@ -6,15 +6,12 @@ cd ${FRONTEND_PATH} && \
 node_modules/@angular/cli/bin/ng build --prod
 echo
 echo ------------- Creation and Copying to bucket
-aws s3 $AWS_PROFILE website s3://$S3_FRONTEND_BUCKET_NAME --index index.html --error index.html
 aws s3 $AWS_PROFILE rm s3://$S3_FRONTEND_BUCKET_NAME --recursive
 aws s3 $AWS_PROFILE cp $FRONTEND_BUILD_PATH s3://$S3_FRONTEND_BUCKET_NAME --acl public-read --recursive
-S3_BUCKET_LOCATION=$(aws s3api $AWS_PROFILE get-bucket-location --bucket $S3_FRONTEND_BUCKET_NAME --query LocationConstraint --output text)
 echo
 echo ------------- Output:
-echo S3_BUCKET_LOCATION      $S3_BUCKET_LOCATION
-echo "View your project here: http://$S3_FRONTEND_BUCKET_NAME.s3-website.$S3_BUCKET_LOCATION.amazonaws.com"
-echo "View your project here: http://$S3_FRONTEND_BUCKET_NAME.s3-website.$S3_BUCKET_LOCATION.amazonaws.com" > $CURRENT_DIR/result.frontend_deploy.json
+echo "View your project here: http://$S3_FRONTEND_BUCKET_NAME.s3-website-$AWS_REGION.amazonaws.com"
+echo "View your project here: http://$S3_FRONTEND_BUCKET_NAME.s3-website-$AWS_REGION.amazonaws.com" > $CURRENT_DIR/result.frontend_deploy.json
 echo
 echo ------------- Check:
 set -x;
---
+++ infra/webapi_deploy.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+source variables.sh
+
+ecs_cluster_create() {
+    echo $LINE Create Amazon ECS Cluster
+    set -x;
+    aws ecs $AWS_PROFILE create-cluster --cluster-name $PROJECT_NAME-Cluster
+    set +x;
+    echo
+}
+ecs_cluster_destroy() {
+    echo $LINE Destroy Amazon ECS Cluster
+    set -x;
+    aws ecs $AWS_PROFILE delete-cluster --cluster $PROJECT_NAME-Cluster
+    set +x;
+    echo
+}
+
+cloudwatch_logs_group_create() {
+    echo $LINE Create CloudWatch Logs Group
+    set -x;
+    aws logs $AWS_PROFILE create-log-group --log-group-name $PROJECT_NAME-logs
+    set +x;
+    echo
+}
+cloudwatch_logs_group_destroy() {
+    echo $LINE Destroy CloudWatch Logs Group
+    set -x;
+    aws logs $AWS_PROFILE delete-log-group --log-group-name $PROJECT_NAME-logs
+
+    set +x;
+    echo
+}
+
+ecs_cluster_create
+cloudwatch_logs_group_create
