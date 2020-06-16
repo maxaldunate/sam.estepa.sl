fc5bcd295d3ff3b67031e8e19ace6ee73b6a97db
maxaldunate <maxaldunate@gmail.com>
2020-06-16 22:43:23 +0200

    frontend to API

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MysticalMysfitProfileGridComponent } from './mystical-mysfit-profile-grid/mystical-mysfit-profile-grid.component';
@@ -11,7 +12,8 @@ import { MythicalMysfitProfileService } from './services/mythical-mysfit-profile
     MysticalMysfitProfileGridComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [MythicalMysfitProfileService],
   bootstrap: [AppComponent]
